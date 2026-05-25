ff4fa1d3b405aef6973fa75cafa4b5ad5c62ce85
rola <52330965@students.liu.edu.lb>
2026-05-25 19:01:08 +0300

    in booking page we use the useState for calculate totalprices we have two useState the first for zones we have and second for hours

--- src/components/booking.jsx
+++ src/components/booking.jsx
@@ -1,4 +1,12 @@
-import React from 'react'
+import { useState } from "react"
 
+
+
+const booking=()=>{
+  const [zone,setzone]=useState("A");
+  const[hours,sethours]=useState(1);
+  const prices=zone==="A"?2 :zone==="B"? 3:4;
+  const total=hours*prices;
+}
 
 export default booking
